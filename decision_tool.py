6a3c47070ff9d93f45e8dd5d2d9f1303d48b1e2e
MarryLou0 <marika.gegotek99@gmail.com>
2022-08-29 11:46:50 +0200

    updated conditionals

--- decision_tool.py
+++ decision_tool.py
@@ -4,8 +4,8 @@ from random import choice
 # group 1 is fast and easy to get,
 # group 2 is the most healthy one
 
-foods = [['pizza', 'gnocchi', 'pasta', 'tiramisu', 'dumplings', 'ramen'], ['tacos', 'tom yum' 'burrito', 'quesadilla',
-                                                                           'pad thai'],
+foods = [['pizza', 'gnocchi', 'pasta', 'tiramisu', 'dumplings', 'ramen'],
+         ['tacos', 'tom yum', 'burrito', 'quesadilla', 'pad thai'],
          ['salad', 'sushi', 'stir fry', 'tofu with rice', 'veg smoothie']]
 
 # greet the user
@@ -15,7 +15,7 @@ mood = int(input("Please rate your today's mood in scale from 0 to 5: "))
 # choose from mood groups
 if mood < 2:
     print(f'Okay, poor thing, here is something you may like: {choice(foods[0])}')
-elif mood ==2 or mood ==3:
+elif 2<= mood <= 3:
     print(f'Not great, not terrible, huh? Here you go: {choice(foods[1])}')
 elif mood >3:
     print(f'Good for you! Good mood means good food: {choice(foods[2])}')
