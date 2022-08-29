127c789713fefbc203ad2c3ac0c8f92cc5194f61
MarryLou0 <marika.gegotek99@gmail.com>
2022-08-29 13:42:01 +0200

    added exception and loop

--- decision_tool.py
+++ decision_tool.py
@@ -10,15 +10,24 @@ foods = [['pizza', 'gnocchi', 'pasta', 'tiramisu', 'dumplings', 'ramen'],
 
 # greet the user
 print("Hello! It's time to choose best food for now :)")
-mood = int(input("Please rate your today's mood in scale from 0 to 5: "))
+
 
 # choose from mood groups
-if mood < 2:
-    print(f'Okay, poor thing, here is something you may like: {choice(foods[0])}')
-elif 2<= mood <= 3:
-    print(f'Not great, not terrible, huh? Here you go: {choice(foods[1])}')
-elif mood >3:
-    print(f'Good for you! Good mood means good food: {choice(foods[2])}')
+while True:
+    try:
+        mood = int(input("Please rate your today's mood in scale from 0 to 5: "))
+        if mood < 2:
+            print(f'Okay, poor thing, here is something you may like: {choice(foods[0])}')
+            break
+        elif 2<= mood <= 3:
+            print(f'Not great, not terrible, huh? Here you go: {choice(foods[1])}')
+            break
+        elif mood >3:
+            print(f'Good for you! Good mood means good food: {choice(foods[2])}')
+            break
+    except ValueError:
+        print('Seems not quite right, could you rate your mood in proper scale 0-5? ')
+
 
 
 
